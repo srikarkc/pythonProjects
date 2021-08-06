e746ab99d3e13f1d3c308594e5d223ba0ac8a6a4
Srikar <sclifeisgreat@gmail.com>
2021-08-06 19:56:00 +0000

    Added bandnameGenerator

---
+++ bandnameGenerator.py
@@ -0,0 +1,4 @@
+print('Welcome to the band name generator program.\n')
+city_name = input('Enter the name of the city you were born in. \n')
+pet_name = input('Enter the name of your pet. \n')
+print('Your band name could be \'' + city_name + ' ' + pet_name + '\'.')
